78df67f2fe75f3a23d54200b17e21642f1e7b5d7
hylim <hylim@zalesia.co.kr>
2023-08-06 23:33:24 +0900

    section02

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,10 +1,12 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -25,11 +27,27 @@ public class HelloController {
         return "hello-template";
     }
 
-//    @GetMapping("hello-string")
-//    @RequestBody
-//    public String helloString(@RequestParam("name") String name){
-//        return "hello" + name;
-//    }
+    @GetMapping("hello-string")
+    @RequestBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
 
 
 }
