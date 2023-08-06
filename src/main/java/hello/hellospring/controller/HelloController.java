389ea5803c8c087c4f3bdc96c01a27fe7fc7566c
hyim <younge1009@gmail.com>
2023-08-06 23:44:59 +0900

    test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -44,9 +44,9 @@ public class HelloController {
             return name;
         }
 
-        public void setName(String name){
-            this.name = name;
-        }
+//        public void setName(String name){
+//            this.name = name;
+//        }
     }
 
 
